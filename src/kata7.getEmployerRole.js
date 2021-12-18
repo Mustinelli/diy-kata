d09606ae589f320c206a8afcf97676c5852cee9f
Mustafa Abdillahi <mustafa.abdi1@outlook.com>
2021-12-18 18:43:17 +0000

    final results

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,3 +1,6 @@
-const getEmployerRole = (employeeName, employees) => {};
+const getEmployerRole = (employeeName, employees) => {
+  const person = employees.find(employee => employee.name === employeeName);
+  return person.role;
+};
 
 module.exports = getEmployerRole;
